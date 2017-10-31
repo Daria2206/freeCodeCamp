595fe807ce7ceb071a587f10f5d35f210cc5c30b
Daria Juchnowicz <daria.juchnowicz@gmail.com>
2017-10-31 17:12:17 +0100

    Sorted Union challenge added.

--- Intermediate_Algorithm_Scripting/script.js
+++ Intermediate_Algorithm_Scripting/script.js
@@ -95,6 +95,43 @@ console.log("");
 
 /*
 ==============================================================================
+Sorted Union
+Write a function that takes two or more arrays and returns a new array of 
+unique values in the order of the original provided arrays. In other words, 
+all values present from all arrays should be included in their original order, 
+but with no duplicates in the final array. The unique numbers should be sorted 
+by their original order, but the final array should not be sorted in numerical 
+order.
+==============================================================================
+*/ 
+
+function uniteUnique(arr) {
+  "use strict";
+  var values = [].concat.apply([], Array.from(arguments));
+  var alreadyThere = {};
+  var sansDuplicates = [];
+  for (var i=0; i<values.length; ++i) {
+    if (!(values[i] in alreadyThere)) {
+      alreadyThere[values[i]] = null;
+      sansDuplicates.push(values[i]);
+    }
+  }
+  return sansDuplicates;
+}
+
+
+// Some tests
+console.log("Sorted Union");
+console.log(uniteUnique([1, 3, 2], [5, 2, 1, 4], [2, 1])); // should return [1, 3, 2, 5, 4].
+console.log(uniteUnique([1, 3, 2], [1, [5]], [2, [4]])); // should return [1, 3, 2, [5], [4]].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1])); // should return [1, 2, 3, 5].
+console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])); // should return [1, 2, 3, 5, 4, 6, 7, 8].
+console.log("");
+
+
+
+/*
+==============================================================================
 END OF INTERMEDIATE ALGORITHMS SCRIPTING
 ==============================================================================
 */

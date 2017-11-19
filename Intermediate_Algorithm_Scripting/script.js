2fb51eb98d3d9fa6c2190eba767738ea68afb194
Daria Juchnowicz <daria.juchnowicz@gmail.com>
2017-11-19 12:07:14 +0100

    Pig Latin func.

--- Intermediate_Algorithm_Scripting/script.js
+++ Intermediate_Algorithm_Scripting/script.js
@@ -3,6 +3,39 @@ My code for JavaScript challanges from freeCodeCamp (Intermediate Algorithm
  Scripting).
  */
 
+/*
+==============================================================================
+Pig Latin 
+Translate the provided string to pig latin.
+Pig Latin takes the first consonant (or consonant cluster) of an English word, 
+moves it to the end of the word and suffixes an "ay".
+If a word begins with a vowel you just add "way" to the end.
+Input strings are guaranteed to be English words in all lowercase.
+==============================================================================
+*/
+
+function translatePigLatin(str) {
+  var vowelsBeginning = /^[aeoiuy]/;
+  var vowels = /[aeoiuy]/;
+  var firstVowel;
+  if (vowelsBeginning.test(str)) {
+    return str + "way";
+  }
+  else {
+    firstVowel = str.search(vowels); 
+    return str.slice(firstVowel) + str.slice(0, firstVowel) + "ay";
+  }
+}
+
+// Some tests
+console.log("Pig Latin");
+console.log(translatePigLatin("california")); // should return "aliforniacay".
+console.log(translatePigLatin("paragraphs")); // should return "aragraphspay".
+console.log(translatePigLatin("glove")); // should return "oveglay".
+console.log(translatePigLatin("algorithm")); // should return "algorithmway".
+console.log(translatePigLatin("eight")); // should return "eightway".
+console.log('');
+
 
 /*
 ==============================================================================

734f7a9a40d3be6f8d2a46506133aa82a17413a8
Daria Juchnowicz <daria.juchnowicz@gmail.com>
2017-11-19 11:59:39 +0100

    HTML converter func.

--- Intermediate_Algorithm_Scripting/script.js
+++ Intermediate_Algorithm_Scripting/script.js
@@ -129,6 +129,38 @@ console.log(uniteUnique([1, 2, 3], [5, 2, 1, 4], [2, 1], [6, 7, 8])); // should 
 console.log("");
 
 
+/*
+==============================================================================
+Convert HTML Entities
+Convert the characters &, <, >, " (double quote), and ' (apostrophe), in a 
+string to their corresponding HTML entities.
+==============================================================================
+*/ 
+
+const replaceChars = {
+  "&": "&amp;",
+  "<": "&lt;",
+  ">": "&gt;",
+  "\"": "&quot;",
+  "'": "&apos;"
+};
+
+function convertHTML(str) {
+    return str.replace(/&|<|>|'|"/g,function(match) {return replaceChars[match];});
+}
+
+// Some tests
+console.log('convertHTML');
+console.log(convertHTML("Dolce & Gabbana")); // should return Dolce &​amp; Gabbana.
+console.log(convertHTML("Hamburgers < Pizza < Tacos")); // should return Hamburgers &​lt; Pizza &​lt; Tacos.
+console.log(convertHTML("Sixty > twelve")); // should return Sixty &​gt; twelve.
+console.log(convertHTML('Stuff in "quotation marks"')); // should return Stuff in &​quot;quotation marks&​quot;.
+console.log(convertHTML("Shindler's List")); // should return Shindler&​apos;s List.
+console.log(convertHTML("<>")); // should return &​lt;&​gt;.
+console.log(convertHTML("abc")); // should return abc.
+console.log('');
+
+
 
 /*
 ==============================================================================
